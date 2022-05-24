8c3f290048740c3aeaf853e4df88e00af1cd4b03
josbuc27 <88007860+josbuc27@users.noreply.github.com>
2022-05-24 13:25:06 +1200

    test/player
    added test lvl and started player script

---
+++ Scences/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody
+
+var velocity = Vector3()
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func get_input():
+	pass
+	
+func _unhandled_input(event):
+	pass
+
+func
+
+
