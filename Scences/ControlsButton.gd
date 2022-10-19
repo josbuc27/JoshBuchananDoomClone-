401b9f508e4aa61bee39c9c2fc2ad083d3cc5e7a
josbuc27 <88007860+josbuc27@users.noreply.github.com>
2022-10-19 13:23:58 +1300

    Title Screen edits
    Added Background to the title screen and the option screen

--- Scences/ControlsButton.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-
-
-func _on_ControlsButton_pressed():
-	get_tree().change_scene("res://Scences/Options.tscn")
-	print("button pressed")
---
+++ Scripts/GoBackBUtton.gd
@@ -0,0 +1,8 @@
+extends Button
+
+
+
+
+func _on_GoBackBUtton_pressed():
+	get_tree().change_scene("res://Scences/Title Screen.tscn")
+	print("button pressed")
